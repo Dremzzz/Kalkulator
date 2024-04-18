4992106a920a9e436a7a5285198f8216ef6d758e
Dremz <160274050+Dremzzz@users.noreply.github.com>
2024-04-18 21:44:12 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,15 @@
-let btns = document.getElementsByClassName("btn");
-let btnsCount = btns.length - 1;
-
 let wynik = document.getElementById("wynik");
 
 function button(n) {
     if (n == 0) {
+        wynik.value += ``;
+    } else if (n == 1) {
         wynik.value = eval(wynik.value);
         wynik.value = Math.sqrt(wynik.value);
-    } else if (n == 1) {
-        wynik.value += `%`;
     } else if (n == 2) {
         wynik.value = ``;
     } else if (n == 3) {
-        wynik.value += `/`;
+        check(`/`);
     } else if (n == 4) {
         wynik.value += `7`;
     } else if (n == 5) {
@@ -20,7 +17,7 @@ function button(n) {
     } else if (n == 6) {
         wynik.value += `9`;
     } else if (n == 7) {
-        wynik.value += `*`;
+        check(`*`);
     } else if (n == 8) {
         wynik.value += `4`;
     } else if (n == 9) {
@@ -28,7 +25,7 @@ function button(n) {
     } else if (n == 10) {
         wynik.value += `6`;
     } else if (n == 11) {
-        wynik.value += `-`;
+        check(`-`);
     } else if (n == 12) {
         wynik.value += `1`;
     } else if (n == 13) {
@@ -36,16 +33,38 @@ function button(n) {
     } else if (n == 14) {
         wynik.value += `3`;
     } else if (n == 15) {
-        wynik.value += `+`;
+        check(`+`);
     } else if (n == 16) {
         wynik.value += ``;
     } else if (n == 17) {
         wynik.value += `0`;
     } else if (n == 18) {
-        wynik.value += `.`;
+        check(`.`);
     } else if (n == 19) {
         wynik.value = eval(wynik.value);
     } else {
         console.log("ERROR");
     }
+}
+
+function check(o) {
+    let index = strLen(wynik.value)-1;
+    let w = wynik.value[index];
+    if (index < 0) {
+        index += 1;
+    } else {
+        if (w == "/" || w == "*" || w == "-" || w == "+" || w == "%") {
+            wynik.value += ``;
+        } else {
+            wynik.value += o;
+        }
+    }
+}
+
+function strLen(str) {
+    let count = 0;
+    for (let i = 0, len = str.length; i < len; i++) {
+        count += str.charCodeAt(i) < 256 ? 1 : 2;
+    }
+    return count;
 }
